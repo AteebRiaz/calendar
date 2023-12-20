eba5afcec8bbf5e38c84fd2f4311c121d038d727
AteebRiaz <ateebriaz92@gmail.com>
2023-12-20 16:58:39 +0500

    update

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const EventList = ({ selectedDate, events }) => {
   return (
     <div className="event-list">
-      <h2>Selected Date: {selectedDate}</h2>
+      <h2>Event at : <br></br>{selectedDate}</h2>
       <ul>
         {events
           .filter((event) => event.date === selectedDate)
