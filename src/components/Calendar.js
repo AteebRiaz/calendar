78150e8a0eaa43c0a1f4ab9aaba3f6fb008330da
AteebRiaz <ateebriaz92@gmail.com>
2023-12-20 15:23:28 +0500

    update

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,6 +7,12 @@ import '../styles/Calendar.css';
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [events, setEvents] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  const getDaysInMonth = (month, year) => {
+    return new Date(year, month, 0).getDate();
+  };
 
   const handleDateClick = (date) => {
     setSelectedDate(date);
@@ -18,7 +24,7 @@ const Calendar = () => {
 
   const getEventPreviews = (date) => {
     const dateEvents = events.filter((event) => event.date === date);
-    const eventPreviews = dateEvents.slice(0, 2); // Show only the first two events as previews
+    const eventPreviews = dateEvents.slice(0, 2);
 
     if (dateEvents.length > 2) {
       const remainingCount = dateEvents.length - 2;
@@ -28,24 +34,76 @@ const Calendar = () => {
     return eventPreviews;
   };
 
+  const renderCalendar = () => {
+    const daysInMonth = getDaysInMonth(currentMonth, currentYear);
+    const currentDate = new Date(currentYear, currentMonth - 1, 1);
+    const startingDay = currentDate.getDay() || 7; // Adjust to start from Monday (1)
+  
+    return (
+      <div className="calendar">
+        {Array.from({ length: daysInMonth + startingDay - 1 }).map((_, index) => {
+          const day = index + 1 - startingDay + 1;
+          return day > 0 ? (
+            <CalendarCell
+              key={index}
+              date={day}
+              onClick={() => handleDateClick(day)}
+              eventPreviews={getEventPreviews(day)}
+            />
+          ) : (
+            <div key={index} className="empty-cell" />
+          );
+        })}
+      </div>
+    );
+  };
+  
+  
+
+  const handleNextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth % 12) + 1);
+    if (currentMonth === 12) {
+      setCurrentYear((prevYear) => prevYear + 1);
+    }
+  };
+
+  const handlePrevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
+    if (currentMonth === 1) {
+      setCurrentYear((prevYear) => prevYear - 1);
+    }
+  };
+
+  const handleNextYear = () => {
+    setCurrentYear((prevYear) => prevYear + 1);
+  };
+
+  const handlePrevYear = () => {
+    setCurrentYear((prevYear) => prevYear - 1);
+  };
+
   return (
     <div className="calendar-container">
       <div className="calendar-section">
-        <div className="calendar">
-          {[...Array(31)].map((_, index) => (
-            <CalendarCell
-              key={index}
-              date={index + 1}
-              onClick={() => handleDateClick(index + 1)}
-              eventPreviews={getEventPreviews(index + 1)}
-            />
+        <div className="month-year">
+          <button onClick={handlePrevYear}>&lt;&lt;</button>
+          <button onClick={handlePrevMonth}>&lt;</button>
+          <h2>{`${new Date(currentYear, currentMonth - 1).toLocaleDateString('en-US', {
+            month: 'long',
+          })} ${currentYear}`}</h2>
+          <button onClick={handleNextMonth}>&gt;</button>
+          <button onClick={handleNextYear}>&gt;&gt;</button>
+        </div>
+        <div className="day-labels">
+          {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map((day, index) => (
+            <div key={index} className="day-label">{day}</div>
           ))}
         </div>
+        {renderCalendar()}
       </div>
 
       <div className="event-section">
         <EventList selectedDate={selectedDate} events={events} />
-
         {selectedDate && (
           <AddEventForm
             selectedDate={selectedDate}
