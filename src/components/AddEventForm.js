0e484845d57b8a81899e8fb4269b41c2f7c14ea4
AteebRiaz <ateebriaz92@gmail.com>
2023-12-30 14:29:27 +0500

    update

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 
 const AddEventForm = ({ selectedDate, onAddEvent }) => {
   const [eventName, setEventName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Check if eventName is not empty
+    if (!eventName.trim()) {
+      setErrorMessage('Event Name cannot be empty');
+      return;
+    }
+
+    // Clear any previous error messages
+    setErrorMessage('');
+
+    // Call onAddEvent only if eventName is not empty
     onAddEvent({ eventName });
     setEventName('');
   };
@@ -22,6 +34,7 @@ const AddEventForm = ({ selectedDate, onAddEvent }) => {
           />
         </label>
         <button type="submit">Save</button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
       </form>
     </div>
   );
